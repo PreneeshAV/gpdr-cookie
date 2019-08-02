044a41d0b39678a939c6b753db1fef430dcc714a
preneesh <preneesh@dreamstertech.com>
2019-08-02 14:12:54 +0530

    initial commit

---
+++ gpdr.js
@@ -0,0 +1,11 @@
+/**
+Website : https://www.speaktech.in
+Author : preneesh av
+**/
+
+$.cookieMessage({
+    'mainMessage': 'This website uses cookies. By using this website you consent to our use of these cookies. For more information visit our <a href="https://www.dreamstertech.com/privacy/">Privacy Policy</a>. ',
+    'acceptButton': 'Got It!',
+    'fontSize': '10px',
+    'backgroundColor': 'rgb(38, 56, 73)',
+});
